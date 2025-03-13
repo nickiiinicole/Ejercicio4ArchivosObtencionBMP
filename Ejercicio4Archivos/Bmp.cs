cb97b27756943014763b7beecb23729fd6898559
nicki <nickiiitadc4@gmail.com>
2025-03-13 10:01:05 +0100

    cambio hechi

--- Ejercicio4Archivos/Bmp.cs
+++ Ejercicio4Archivos/Bmp.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Runtime.Remoting.Messaging;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Ejercicio4Archivos
 {
@@ -15,59 +11,97 @@ namespace Ejercicio4Archivos
         public Bmp(string path)
         {
             this.pathBmp = path;
-
         }
 
-        public bool IsHeaderBmp(string pathBmp)
-        {
-            using (FileStream fs = new FileStream(pathBmp, FileMode.Open))
-            {
-                //porque los primeros bytes indican que es La B y M 
-                string format = ((char)fs.ReadByte()).ToString() + ((char)fs.ReadByte()).ToString());
-                if (format == "BM")
-                {
-                    return true;
-                }
-                return false;
-            }
-
-        }
-        public bool SizeCorrect(string pathBmp)
-        {
-
-            using (BinaryReader reader = new BinaryReader(new FileStream(pathBmp, FileMode.Open)))
-            {
-                //me pongo en la posicion 2 y puedo leer directamente un integer en este caso 
-                reader.BaseStream.Seek(2, SeekOrigin.Begin);
-                if (reader.ReadInt32() == reader.BaseStream.Length)
-                {
-                    return true;
-                }
-
-            }
-            return false;
-        }
-        public bool IsBmp(string pathBmp)
+        // Método para verificar si el archivo es BMP
+        public bool IsHeaderBmp()
         {
             if (!File.Exists(pathBmp))
             {
                 return false;
             }
-            if (!IsHeaderBmp(pathBmp) && !SizeCorrect(pathBmp))
-            {
 
-                return false;
+            try
+            {
+                using (FileStream fs = new FileStream(pathBmp, FileMode.Open, FileAccess.Read))
+                {
+                    // Leer los primeros dos bytes para verificar la cabecera "BM"
+                    byte[] header = new byte[2];
+                    fs.Read(header, 0, 2);
+                    string format = Encoding.ASCII.GetString(header);
+                    return format == "BM";
+                }
             }
-            return true;
+            catch (Exception e) when (e is ArgumentException || e is ArgumentNullException)
+            {
+                Console.WriteLine($"{e.Message}");
+            }
+            return false;
         }
 
-        public object InfoBmp(string pathBmp)
+        public bool SizeCorrect()
         {
-
-            using (BinaryReader reader = new BinaryReader(new FileStream(pathBmp, FileMode.Open)))
+            try
             {
-
+                using (BinaryReader reader = new BinaryReader(new FileStream(pathBmp, FileMode.Open)))
+                {
+                    reader.BaseStream.Seek(2, SeekOrigin.Begin); // Posicionarse en el byte 2
+                    int fileSize = reader.ReadInt32(); // Leer el tamaño del archivo
+                    return fileSize == reader.BaseStream.Length;
+                }
             }
+            catch (Exception e) when (e is IOException || e is ArgumentException)
+            {
+                Console.WriteLine($"{e.Message}");
+            }
+            return false;
+        }
+
+        public bool IsBmp()
+        {
+            if (!File.Exists(pathBmp))
+            {
+                return false;
+            }
+
+            return IsHeaderBmp() && SizeCorrect();
+        }
+
+        public object InfoBmp()
+        {
+            try
+            {
+                using (BinaryReader reader = new BinaryReader(new FileStream(pathBmp, FileMode.Open)))
+                {
+                    reader.BaseStream.Seek(14, SeekOrigin.Begin);
+
+                    int width = reader.ReadInt32();
+                    int height = reader.ReadInt32();
+                    short planes = reader.ReadInt16();
+                    short bpp = reader.ReadInt16();
+                    int compression = reader.ReadInt32();
+                    reader.BaseStream.Seek(38, SeekOrigin.Begin);
+                    int resolutionX = reader.ReadInt32();
+                    int resolutionY = reader.ReadInt32();
+
+
+                    return new
+                    {
+                        Width = width,
+                        Height = height,
+                        IsCompressed = compression == 0,
+                        BitsPerPixel = bpp,
+                        HorizontalResolution = resolutionX,
+                        VerticalResolution = resolutionY
+                    };
+                }
+            }
+            catch (Exception e) when (e is IOException || e is ArgumentException)
+            {
+                Console.WriteLine($"Error al leer la información del BMP: {e.Message}");
+            }
+
+            return null;
         }
     }
 }
